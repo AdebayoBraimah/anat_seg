3825b5c5d0e50fbec78a1bfaba5ed7e2b61f7cbd
AdebayoBraimah <adebayo.braimah@gmail.com>
2022-05-08 23:37:44 -0400

    MNT: Added script to automatically update submodules when called

---
+++ .dev/submodules.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# TODO:
+#   * Clone submodules for separate branch/(version) tag
+#   * Automaticaly clone submodules when cloning git repo.
+
+# Update git submodules in project
+git submodule update --remote --merge
